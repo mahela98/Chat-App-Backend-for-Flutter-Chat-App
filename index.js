93c6e84ccb963e87bec42993e4091676a4b95ad2
mahela98 <chandimaofficial@gmail.com>
2021-11-11 22:17:55 +0530

    basic server

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+var server = http.createServer(app);
+
+var io = require("socket.io")(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+//middleware
+
+app.use(express.json());
+app.use(cors());
+
+io.on("Connection", (socket) => {
+    console.log("connected");
+});
+
+server.listen(port, () => {
+    console.log("server started");
+
+});
