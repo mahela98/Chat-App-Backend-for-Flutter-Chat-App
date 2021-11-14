50d9cc4a952eaee88fc02f1ebb841471e15ae63d
mahela98 <chandimaofficial@gmail.com>
2021-11-14 09:07:07 +0530

    changed socketio version

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
-
 const app = express();
 const port = process.env.PORT || 5000;
-
 var server = http.createServer(app);
-
-var io = require("socket.io")(server, {
-    cors: {
-        origin: "*"
-    }
-});
+var io = require("socket.io")(server);
 
 //middleware
-
 app.use(express.json());
 app.use(cors());
 
-io.on("Connection", (socket) => {
+io.on("connection", (socket) => {
     console.log("connected");
 });
 
 server.listen(port, () => {
     console.log("server started");
-
 });
